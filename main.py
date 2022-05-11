9c757d5e12bb400110f95831c2043d5cc7396f35
Dvuglaf <aizeensan@gmail.com>
2022-05-11 14:05:02 +0400

    Trying to fix loss_3...

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 from skimage.io import imshow, show
 
-physical_devices = tf.config.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
 os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v11.2/bin")
+
+physical_devices = tf.config.list_physical_devices('GPU')
+for device in physical_devices:
+    tf.config.experimental.set_memory_growth(device, True)
 
 print("Num GPUs Available: ", len(tensorflow.config.list_physical_devices('GPU')))
 
@@ -29,26 +30,8 @@ def rgb2gray(rgb):
 
 
 def conv_block_b(input_tensor):
-
-    top = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(input_tensor)
-
-    mid = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(input_tensor)
-    mid = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=mid.shape[1:])(mid)
-
-    bot = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(input_tensor)
-    bot = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=bot.shape[1:])(bot)
-    bot = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=bot.shape[1:])(bot)
-
-    concat = keras.layers.Concatenate(axis=len(input_tensor.shape) - 1)([top, mid, bot])
-    concat = keras.layers.Conv2D(input_tensor.shape[3], (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(concat)
-    res = concat + input_tensor
-    return res
-
-
-def conv_block_b_star(input_tensor):
     top = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(
         input_tensor)
-
 
     mid = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(
         input_tensor)
@@ -58,13 +41,36 @@ def conv_block_b_star(input_tensor):
         input_tensor)
     bot = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=bot.shape[1:])(bot)
     bot = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=bot.shape[1:])(bot)
-    concat = keras.layers.Concatenate(axis=len(input_tensor.shape) - 1, name='b_1')([top, mid, bot])
-    b_1 = copy.copy(concat)
-    concat = keras.layers.Conv2D(input_tensor.shape[3], (1, 1), padding='same', activation='relu', name='b_2',
+
+    concat = keras.layers.Concatenate(axis=len(input_tensor.shape) - 1)([top, mid, bot])
+    concat = keras.layers.Conv2D(input_tensor.shape[3], (1, 1), padding='same', activation='relu',
                                  input_shape=input_tensor.shape[1:])(concat)
-    b_2 = copy.copy(concat)
     res = concat + input_tensor
-    return res, b_1, b_2
+    return res
+
+
+def conv_block_b_star(input_tensor):
+    top = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(
+        input_tensor)
+
+    mid = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(
+        input_tensor)
+    mid = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=mid.shape[1:])(mid)
+
+    bot = keras.layers.Conv2D(32, (1, 1), padding='same', activation='relu', input_shape=input_tensor.shape[1:])(
+        input_tensor)
+    bot = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=bot.shape[1:])(bot)
+    bot = keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=bot.shape[1:])(bot)
+
+    concat = keras.layers.Concatenate(axis=len(input_tensor.shape) - 1)([top, mid, bot])
+
+    b1 = concat
+
+    b2 = keras.layers.Conv2D(input_tensor.shape[3], (1, 1), padding='same', activation='relu',
+                              input_shape=input_tensor.shape[1:])(b1)
+
+    res = b2 + input_tensor
+    return res, b1, b2
 
 
 # Inputs
@@ -81,20 +87,21 @@ x = keras.layers.Conv2D(48, (1, 1), padding='same', name='1', activation='relu',
 encoder_output = tensorflow.reshape(x, [-1, 128, 128, 3])
 
 # Embedder
-x, b_1_w, b_2_w = conv_block_b_star(encoder_output)
+x, b1w, b2w = conv_block_b_star(encoder_output)
 x = keras.layers.Concatenate(axis=len(x.shape) - 1)([x, coverimage])
 x = conv_block_b(x)
 embedder_output = keras.layers.Conv2D(3, (1, 1), padding='same', activation='relu', input_shape=x.shape[1:])(x)
 
-
 # Invariance network
 N = 5
 invariance_output = keras.layers.Dense(N, activation='relu', name='3')(embedder_output)
+#add = conv_block_b_star(embedder_output)
 
 # Extractor
 x = conv_block_b(invariance_output)
 x = conv_block_b(x)
-extractor_output = keras.layers.Conv2D(3, (1, 1), padding='same', name='4', activation='relu', input_shape=x.shape[1:])(x)
+extractor_output = keras.layers.Conv2D(3, (1, 1), padding='same', name='4', activation='relu', input_shape=x.shape[1:])(
+    x)
 
 # Decoder
 x = tensorflow.reshape(extractor_output, [-1, 32, 32, 48])
@@ -104,6 +111,8 @@ x = keras.layers.Conv2D(24, (1, 1), padding='same', activation='relu', input_sha
 x = conv_block_b(x)
 x = conv_block_b(x)
 decoder_output = keras.layers.Conv2D(1, (1, 1), padding='same', name='5', activation='relu', input_shape=x.shape[1:])(x)
+f, b1m, b2m = conv_block_b_star(embedder_output)
+
 # Single network model
 network = keras.Model([watermark, coverimage], [decoder_output, embedder_output], name="network")
 
@@ -113,28 +122,21 @@ tf.keras.utils.plot_model(
     show_shapes=True,
     show_dtype=True
 )
+
+
 # Loss block
 # m_i       - marked image
 # c_i       - coverimage
 # w_star_i  - extracted and decoded watermark image
 # w_i       - watermark image
+def loss(w, c, w_f, m_i, b_1_m, b_1_w, b_2_m, b_2_w):
+    loss_1 = keras.backend.mean(keras.backend.abs(w_f - w))
+    loss_2 = keras.backend.mean(keras.backend.abs(m_i - c))
 
-
-def loss():
-
-    loss_1 = keras.backend.mean(keras.backend.abs(decoder_output - watermark))
-    loss_2 = keras.backend.mean(keras.backend.abs(embedder_output - coverimage))
-
-    ''' 
-    b_1_w = network.get_layer('b_1').output
-    b_2_w = network.get_layer('b_2').output
-    b_1_m = network.get_layer('b_1_2').output
-    b_2_m = network.get_layer('b_2_2').output
     loss_3 = 0.5 * (
-            tf.keras.backend.mean(tf.keras.backend.abs(gram_matrix(b_1_w) - gram_matrix(b_1_m))) +
-            tf.keras.backend.mean(tf.keras.backend.abs(gram_matrix(b_2_w) - gram_matrix(b_2_m)))
+            keras.backend.mean(keras.backend.abs(gram_matrix(b_1_w) - gram_matrix(b_1_m))) +
+            keras.backend.mean(keras.backend.abs(gram_matrix(b_2_w) - gram_matrix(b_2_m)))
     )
-    '''
     W = K.variable(value=network.get_layer('3').get_weights()[0])  # N x N_hidden
     W = K.transpose(W)  # N_hidden x N
     h = network.get_layer('3').output
@@ -143,7 +145,7 @@ def loss():
     # N_batch x N_hidden * N_hidden x 1 = N_batch x 1
     contractive = 0.01 * K.sum(dh ** 2 * K.sum(W ** 2, axis=1), axis=1)
 
-    return loss_1 + loss_2 + contractive
+    return loss_1 + loss_2 + loss_3 + contractive
 
 
 '''def loss(m_i, c_i, w_star_i, w_i, int_w_1, int_w_2, int_m_1, int_m_2):
@@ -167,9 +169,9 @@ def loss():
     return loss_1 + loss_2 + contractive + loss_3#+ loss_3 + contractive
 '''
 
-network.add_loss(loss())
+#network.add_loss(loss(watermark, coverimage, decoder_output, embedder_output, b1m, b1w, b2m, b2w))
 
-network.compile(optimizer='adam')
+network.compile(optimizer='adam', loss='mae')
 # Training datasets
 #   Watermark (cifar10, 32x32x1)
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
@@ -178,34 +180,39 @@ x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = rgb2gray(x_train)
 x_test = rgb2gray(x_test)
-x_train = x_train[:-48080]  # to 1920 images
+x_train = x_train[:-49000]  # to 1920 images
 y_train = y_train[:-48080]
 x_test = x_test[:-9520]
 y_test = y_test[:-9520]
-x_train = x_train.reshape((1920, 32, 32, 1))
+x_train = x_train.reshape((1000, 32, 32, 1))
 print(x_train.shape)
 
 #   Coverimage (dataset from internet, 128x128x3)
 # TODO: wrap into function
-train = tf.keras.preprocessing.image_dataset_from_directory("C:/Users/aizee/Desktop/dataset", image_size=(128, 128), shuffle=True, batch_size=1)
-res = []
+train = tf.keras.preprocessing.image_dataset_from_directory("C:/Users/aizee/OneDrive/Desktop/dataset", image_size=(128, 128),
+                                                            shuffle=True, batch_size=1)
+c_train = []
 for element in train.as_numpy_iterator():
-    res.append(element[0]/255)
+    c_train.append(element[0] / 255)
 
-res = np.array(res)
-res = res.reshape((4400, 128, 128, 3))
-res = res[:-2480]
+c_train = np.array(c_train)
+c_train = c_train.reshape((4400, 128, 128, 3))
+c_test = c_train[1920:3840]
+res = c_train[:-3400]
 print(res.shape)
 
 # Model training
-history = network.fit([np.array(x_train), res], [np.array(x_train), res],
+history = network.fit([np.array(x_train), np.array(res)], [np.array(x_train), np.array(res)],
                       epochs=5,
                       batch_size=1,
                       validation_split=0.2,
                       shuffle=True
                       )
-
-
+network.save('full')
+print('\nhistory dict:', history.history)
+# print('\n# Оцениваем на тестовых данных')
+# results = network.evaluate([x_test, c_test], [x_test, c_test], batch_size=3)
+# print('test loss, test acc:', results)
 # Set weights from full network to submodels (encoder + embedder & invariance + extractor + decoder)
 embedder = keras.Model(inputs=[watermark, coverimage], outputs=[embedder_output], name="embedder")
 embedder.compile(optimizer="adam")
@@ -221,32 +228,154 @@ print("SAVED!")
 
 watermark = x_train[0]
 from skimage.io import imread
-coverimage = imread("C:/Users/aizee/Pictures/x.jpg").astype('float32')/255
-extracted, marked = network.predict([np.array(watermark).reshape((1, 32, 32, 1)), np.array(coverimage).reshape((1, 128, 128, 3))], batch_size=1)
-#extracted = extractor.predict(marked, batch_size=1)
-fig = plt.figure()
-sb = fig.add_subplot(2, 3, 1)
-sb.set_title("Исходное изображение")
-imshow(coverimage)
 
-sb = fig.add_subplot(2, 3, 2)
+coverimage_1 = imread("C:/Users/aizee/Pictures/x.jpg").astype('float32') / 255
+coverimage_2 = c_train[1]
+coverimage_3 = c_train[1000]
+coverimage_4 = c_train[3000]
+coverimage_5 = c_train[4000]
+
+extracted_1, marked_1 = network.predict(
+    [np.array(watermark).reshape((1, 32, 32, 1)), np.array(coverimage_1).reshape((1, 128, 128, 3))], batch_size=1)
+extracted_2, marked_2 = network.predict(
+    [np.array(watermark).reshape((1, 32, 32, 1)), np.array(coverimage_2).reshape((1, 128, 128, 3))], batch_size=1)
+extracted_3, marked_3 = network.predict(
+    [np.array(watermark).reshape((1, 32, 32, 1)), np.array(coverimage_3).reshape((1, 128, 128, 3))], batch_size=1)
+extracted_4, marked_4 = network.predict(
+    [np.array(watermark).reshape((1, 32, 32, 1)), np.array(coverimage_4).reshape((1, 128, 128, 3))], batch_size=1)
+extracted_5, marked_5 = network.predict(
+    [np.array(watermark).reshape((1, 32, 32, 1)), np.array(coverimage_5).reshape((1, 128, 128, 3))], batch_size=1)
+
+fig_1 = plt.figure()
+sb = fig_1.add_subplot(2, 3, 1)
+sb.set_title("Исходное изображение")
+imshow(coverimage_1)
+
+sb = fig_1.add_subplot(2, 3, 2)
 sb.set_title("ЦВЗ")
 imshow(watermark)
 
-sb = fig.add_subplot(2, 3, 3)
+sb = fig_1.add_subplot(2, 3, 3)
 sb.set_title("Изображение с ЦВЗ")
-imshow(marked.reshape((128, 128, 3)))
+imshow(marked_1.reshape((128, 128, 3)))
 
-sb = fig.add_subplot(2, 3, 4)
+sb = fig_1.add_subplot(2, 3, 4)
 sb.set_title("Извлеченный ЦВЗ")
-imshow(extracted.reshape((32, 32, 1)), cmap='gray')
+imshow(extracted_1.reshape((32, 32, 1)), cmap='gray')
 
-sb = fig.add_subplot(2, 3, 5)
+sb = fig_1.add_subplot(2, 3, 5)
 sb.set_title("Разница между ЦВЗ и извлеч. ЦВЗ")
-imshow(watermark - extracted.reshape((32, 32, 1)), cmap='gray')
+imshow(watermark - extracted_1.reshape((32, 32, 1)), cmap='gray')
 
-print("Максимальная разница м/у исходным и изобр. с ЦВЗ", np.max(np.abs(coverimage - marked.reshape((128,128,3)))))
-print("Максимальная разница м/у исходным ЦВЗ и извлеченным ЦВЗ", np.max(np.abs(watermark - extracted.reshape((32, 32)))))
+print("Максимальная разница м/у исходным и изобр. с ЦВЗ",
+      np.max(np.abs(coverimage_1 - marked_1.reshape((128, 128, 3)))))
+print("Максимальная разница м/у исходным ЦВЗ и извлеченным ЦВЗ",
+      np.max(np.abs(watermark - extracted_1.reshape((32, 32)))))
+
+fig_2 = plt.figure()
+sb = fig_2.add_subplot(2, 3, 1)
+sb.set_title("Исходное изображение")
+imshow(coverimage_2)
+
+sb = fig_2.add_subplot(2, 3, 2)
+sb.set_title("ЦВЗ")
+imshow(watermark)
+
+sb = fig_2.add_subplot(2, 3, 3)
+sb.set_title("Изображение с ЦВЗ")
+imshow(marked_2.reshape((128, 128, 3)))
+
+sb = fig_2.add_subplot(2, 3, 4)
+sb.set_title("Извлеченный ЦВЗ")
+imshow(extracted_2.reshape((32, 32, 1)), cmap='gray')
+
+sb = fig_2.add_subplot(2, 3, 5)
+sb.set_title("Разница между ЦВЗ и извлеч. ЦВЗ")
+imshow(watermark - extracted_2.reshape((32, 32, 1)), cmap='gray')
+
+print("Максимальная разница м/у исходным и изобр. с ЦВЗ",
+      np.max(np.abs(coverimage_2 - marked_2.reshape((128, 128, 3)))))
+print("Максимальная разница м/у исходным ЦВЗ и извлеченным ЦВЗ",
+      np.max(np.abs(watermark - extracted_2.reshape((32, 32)))))
+
+fig_3 = plt.figure()
+sb = fig_3.add_subplot(2, 3, 1)
+sb.set_title("Исходное изображение")
+imshow(coverimage_3)
+
+sb = fig_3.add_subplot(2, 3, 2)
+sb.set_title("ЦВЗ")
+imshow(watermark)
+
+sb = fig_3.add_subplot(2, 3, 3)
+sb.set_title("Изображение с ЦВЗ")
+imshow(marked_3.reshape((128, 128, 3)))
+
+sb = fig_3.add_subplot(2, 3, 4)
+sb.set_title("Извлеченный ЦВЗ")
+imshow(extracted_3.reshape((32, 32, 1)), cmap='gray')
+
+sb = fig_3.add_subplot(2, 3, 5)
+sb.set_title("Разница между ЦВЗ и извлеч. ЦВЗ")
+imshow(watermark - extracted_3.reshape((32, 32, 1)), cmap='gray')
+
+print("Максимальная разница м/у исходным и изобр. с ЦВЗ",
+      np.max(np.abs(coverimage_3 - marked_3.reshape((128, 128, 3)))))
+print("Максимальная разница м/у исходным ЦВЗ и извлеченным ЦВЗ",
+      np.max(np.abs(watermark - extracted_3.reshape((32, 32)))))
+
+fig_4 = plt.figure()
+sb = fig_4.add_subplot(2, 3, 1)
+sb.set_title("Исходное изображение")
+imshow(coverimage_4)
+
+sb = fig_4.add_subplot(2, 3, 2)
+sb.set_title("ЦВЗ")
+imshow(watermark)
+
+sb = fig_4.add_subplot(2, 3, 3)
+sb.set_title("Изображение с ЦВЗ")
+imshow(marked_4.reshape((128, 128, 3)))
+
+sb = fig_4.add_subplot(2, 3, 4)
+sb.set_title("Извлеченный ЦВЗ")
+imshow(extracted_4.reshape((32, 32, 1)), cmap='gray')
+
+sb = fig_4.add_subplot(2, 3, 5)
+sb.set_title("Разница между ЦВЗ и извлеч. ЦВЗ")
+imshow(watermark - extracted_4.reshape((32, 32, 1)), cmap='gray')
+
+print("Максимальная разница м/у исходным и изобр. с ЦВЗ",
+      np.max(np.abs(coverimage_4 - marked_4.reshape((128, 128, 3)))))
+print("Максимальная разница м/у исходным ЦВЗ и извлеченным ЦВЗ",
+      np.max(np.abs(watermark - extracted_4.reshape((32, 32)))))
+
+fig_5 = plt.figure()
+sb = fig_5.add_subplot(2, 3, 1)
+sb.set_title("Исходное изображение")
+imshow(coverimage_5)
+
+sb = fig_5.add_subplot(2, 3, 2)
+sb.set_title("ЦВЗ")
+imshow(watermark)
+
+sb = fig_5.add_subplot(2, 3, 3)
+sb.set_title("Изображение с ЦВЗ")
+imshow(marked_5.reshape((128, 128, 3)))
+
+sb = fig_5.add_subplot(2, 3, 4)
+sb.set_title("Извлеченный ЦВЗ")
+imshow(extracted_5.reshape((32, 32, 1)), cmap='gray')
+
+sb = fig_5.add_subplot(2, 3, 5)
+sb.set_title("Разница между ЦВЗ и извлеч. ЦВЗ")
+imshow(watermark - extracted_5.reshape((32, 32, 1)), cmap='gray')
+
+print("Максимальная разница м/у исходным и изобр. с ЦВЗ",
+      np.max(np.abs(coverimage_5 - marked_5.reshape((128, 128, 3)))))
+print("Максимальная разница м/у исходным ЦВЗ и извлеченным ЦВЗ",
+      np.max(np.abs(watermark - extracted_5.reshape((32, 32)))))
+
 embedder.save("embedder")
 extractor.save("extractor")
 
@@ -254,7 +383,6 @@ show()
 
 exit(111)
 
-#test_scores = network.evaluate([x_train, res], [x_train, res], batch_size=1, verbose=2)
+# test_scores = network.evaluate([x_train, res], [x_train, res], batch_size=1, verbose=2)
 print('Test loss:', test_scores[0])
 print('Test accuracy:', test_scores[1])
-
